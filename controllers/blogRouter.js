5262a6ce1ad9431ed16a0b1d456c4aa291132557
saaba <nathan.albino01@gmail.com>
2025-05-21 18:54:31 -0400

    update tests

--- controllers/blogRouter.js
+++ controllers/blogRouter.js
@@ -13,4 +13,11 @@ blogRouter.post("/", async (request, response) => {
   response.status(201).json(result);
 });
 
+blogRouter.delete("/:id", async (request, response) => {
+  const id = request.params.id;
+
+  await Blog.deleteOne({ _id: id });
+  response.status(204).send();
+});
+
 module.exports = blogRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -10,12 +10,7 @@ const api = supertest(app);
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-
-  let blogObject = new Blog(helper.initialBlogs[0]);
-  await blogObject.save();
-
-  blogObject = new Blog(helper.initialBlogs[1]);
-  await blogObject.save();
+  await Blog.insertMany(helper.initialBlogs);
 });
 
 test("blogs are return as a json", async () => {
@@ -31,6 +26,33 @@ test("all blogs are returned", async () => {
   assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
 
+test("all blogs have an id named id", async () => {
+  const response = await api.get("/api/blogs");
+
+  const results = response.body.filter((blog) => {
+    const hasIdKey = "id" in blog;
+    const hasUnderscoreIdKey = "_id" in blog; // <-- Negation here
+
+    return !hasIdKey || hasUnderscoreIdKey;
+  });
+
+  assert(results.length === 0);
+});
+
+test("a valid blog post can be deleted", async () => {
+  const contents = await api.get("/api/blogs");
+  const blogObject = contents.body[0];
+
+  await api.delete(`/api/blogs/${blogObject.id}`).expect(204);
+
+  const updatedContents = await api.get("/api/blogs");
+
+  assert.strictEqual(
+    updatedContents.body.length,
+    helper.initialBlogs.length - 1,
+  );
+});
+
 test("a valid blog post can be added", async () => {
   const newPost = {
     title: "a new blog post",
@@ -41,6 +63,14 @@ test("a valid blog post can be added", async () => {
     .send(newPost)
     .expect(201)
     .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/blogs");
+
+  const contents = response.body.map((r) => r.title);
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1);
+
+  assert(contents.includes("a new blog post"));
 });
 
 after(async () => {
